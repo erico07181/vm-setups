79a71dac5b4baf444da9b926b1c8001df81ee7e5
erico07181 <49778407+erico07181@users.noreply.github.com>
2022-04-17 18:20:53 -0600

    Add files via upload

---
+++ centOS-setup.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+yum update
+yum install vim-enhanced -y
+yum install net-tools -y
+
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io
+swapoff -add
+
+yum install apt-transport-https curl
+
+yum install wget
+
+wget https://github.com/kubeedge/kubeedge/releases/download/v1.9.1/keadm-v1.9.1-linux-amd64.tar.gz
+tar -xvsf keadm-v1.9.1-linux-amd64.tar.gz
